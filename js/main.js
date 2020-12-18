6da36eaa3f7edce2913e727c5994064911d45dd2
Vincenzo <enzo.antignani@gmail.com>
2020-12-18 16:55:39 +0100

    Milestone 1 completa - da sistemare overflow delle card

--- js/main.js
+++ js/main.js
@@ -1,1 +1,38 @@
+const app = new Vue (
+  {
+    el: '#root',
+    data: {
+      path: 'https://image.tmdb.org/t/p/w220_and_h330_face/',
+      films: [],
+      text: '',
+    },
+    methods: {
+      searchFilms: function() {
+        var self = this;
 
+
+        axios
+          .get('https://api.themoviedb.org/3/search/movie', {
+            params: {
+              api_key: 'd631125b3120a33333b536f9aa3c36e9',
+              query: this.text,
+              language: 'it-IT'
+              }
+            }
+          )
+          .then(function(response) {
+              self.films = response.data.results;
+
+            }
+          )
+
+        if(this.text == '') {
+          alert("Scrivi cosa vuoi cercare")
+        }
+      }
+    }, //Methods
+
+
+
+  }
+);
