63b64061d61909f0c5847d5cfc79aa46d56cebd2
Vincenzo <enzo.antignani@gmail.com>
2020-12-19 14:12:45 +0100

    sistemato html - css e js (controlli input e card)

--- js/main.js
+++ js/main.js
@@ -5,11 +5,20 @@ const app = new Vue (
       path: 'https://image.tmdb.org/t/p/w220_and_h330_face/',
       films: [],
       text: '',
+      alternative_path: 'https://www.baserunning.it/wp-content/uploads/2020/05/immagine-non-disponibile.png',
+      alternative_overview: 'Descrizione al momento non disponibile',
     },
     methods: {
       searchFilms: function() {
         var self = this;
 
+        //Controllo su input
+        if(this.text == '') {
+          alert("Scrivi cosa vuoi cercare")
+        } else if (this.text.length < 3) {
+          this.text = '';
+          alert("Inserisci almeno tre caratteri")
+        } 
 
         axios
           .get('https://api.themoviedb.org/3/search/movie', {
@@ -26,11 +35,12 @@ const app = new Vue (
             }
           )
 
-        if(this.text == '') {
-          alert("Scrivi cosa vuoi cercare")
-        }
-      }
+
+
+      } // searchFilms
     }, //Methods
+
+
 
 
 
